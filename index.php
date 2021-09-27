49a13a5ef7a54327365d8d28c0f900a09565ea95
jdurdahl <jdurdahl@gmail.com>
2021-09-27 09:14:40 -0400

    working db connection

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <html>
   
   <?php 
-  echo $_SERVER['DOCUMENT_ROOT'];
-  echo "<br><br>";
-  echo __DIR__;
-  echo "<br><br>";
+  // echo $_SERVER['DOCUMENT_ROOT'];
+  // echo "<br><br>";
+  // echo __DIR__;
+  // echo "<br><br>";
   include "./mysql-connect.php"; 
   ?>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"  crossorigin="anonymous">
@@ -18,7 +18,19 @@
           // echo $rootPath;
     ?>
   </head>
-  <body>
+  <body>  
+
+  <?php 
+    // $testString = "hello";
+    // $testInt = 4;
+    // $sqlTest = "INSERT INTO tbl_test (word, num) VALUES (?, ?)";
+    // $stmt = $conn->prepare($sqlTest);
+    // $stmt->bind_param("si", $testString, $testInt);
+
+    // if($stmt->execute()){
+    //   echo "worked <br><br>";
+    // }
+    ?>
       <p>hello sea world</p>
         <p>hello sea world</p> 
         <p>hello sea world</p> 
--- mysql-connect.php
+++ mysql-connect.php
@@ -20,28 +20,28 @@ if(!empty($_ENV['MYSQL_DB'])){
     $db_name = 'seascan';
 }
 
-echo $_ENV['MYSQL_HOST'];
-echo "<br><br>";
-echo "Connecting to Database: $host $user $pass $db_name";
-echo "<br><br>";
+// echo $_ENV['MYSQL_HOST'];
+// echo "<br><br>";
+// echo "Connecting to Database: $host $user $pass $db_name";
+// echo "<br><br>";
 
 $conn = new mysqli($host, $user, $pass, $db_name);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected to MySQL successfully!";
-echo "<br><br>";
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// } 
+// echo "Connected to MySQL successfully!";
+// echo "<br><br>";
 
-$res = $conn->query("Select ITEM_NAME, ITEM_DESC, ITEM_ONHAND from MOE_ITEM_T");
+// $res = $conn->query("Select ITEM_NAME, ITEM_DESC, ITEM_ONHAND from MOE_ITEM_T");
 
-for ($row_no = 0; $row_no < $res->num_rows; $row_no++) {
-    $res->data_seek($row_no);
-    $row = $res->fetch_assoc();
-    echo " Item Name = " . $row['ITEM_NAME'] . " Item Description = " . $row['ITEM_DESC'] . " Item Onhand = " . $row['ITEM_ONHAND'];
-    echo "<br>";
-}
+// for ($row_no = 0; $row_no < $res->num_rows; $row_no++) {
+//     $res->data_seek($row_no);
+//     $row = $res->fetch_assoc();
+//     echo " Item Name = " . $row['ITEM_NAME'] . " Item Description = " . $row['ITEM_DESC'] . " Item Onhand = " . $row['ITEM_ONHAND'];
+//     echo "<br>";
+// }
 
-$res->close();
-$conn->close();
+// $res->close();
+// $conn->close();
 ?>
