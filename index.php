d75efcd994993b6f934ef3ac6c65432e851d8617
jdurdahl <jdurdahl@gmail.com>
2021-10-19 19:38:21 -0400

    added some organization and libraries

--- index.php
+++ index.php
@@ -1,26 +1,44 @@
 <!DOCTYPE html>
-
 <html>
-  
-  <?php 
-  // echo $_SERVER['DOCUMENT_ROOT'];
-  // echo "<br><br>";
-  // echo __DIR__;
-  // echo "<br><br>";
-  include "./mysql-connect.php"; 
-  ?>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"  crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-  <head>
-    <?php 
-    // $rootPath = $_SERVER['DOCUMENT_ROOT'] . "/GitHub/SeaScan";
-          // echo $rootPath;
-    ?>
-  </head>
-  <body>  
 
-  <?php 
+<?php 
+// echo $_SERVER['DOCUMENT_ROOT'];
+// echo "<br><br>";
+echo __DIR__;
+// echo "<br><br>";
+include "./mysql-connect.php"; 
+include "./libraries.html"; 
+?>
+
+<head>
+<?php 
+    // $rootPath = $_SERVER['DOCUMENT_ROOT'] . "/GitHub/SeaScan";
+        // echo $rootPath;
+?>
+</head>
+<script>
+    $(document).ready(function(){
+        $('#navContainer').load('./sideNavBar.php');  
+        $('#contentBody').load('./Home/view_home.php');  
+    });
+</script>
+
+<body id="top">
+<!-- START OF SIDE NAV BAR -->
+
+    <div class="row" id="body-row">
+        <div id="navContainer" class="col-md-2">
+        
+        </div>
+        <div id="contentBody" class="col-md-10">
+        </div>
+    </div>
+</body>
+</html>
+
+
+
+<?php 
     // $testString = "hello";
     // $testInt = 4;
     // $sqlTest = "INSERT INTO tbl_test (word, num) VALUES (?, ?)";
@@ -31,11 +49,3 @@
     //   echo "worked <br><br>";
     // }
     ?>
-      <p>hello sea world</p>
-        <p>hello sea world</p> 
-        <p>hello sea world</p> 
-        <p>hello s  wffd</p> 
-        <p>hello sea world</p> 
-        <button class="btn btn-primary">test</button>
-  </body>
-</html>
