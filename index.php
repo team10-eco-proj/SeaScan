246004b621f081e2bdf32190369d5e7ac612596a
jdurdahl <44073311+jdurdahl@users.noreply.github.com>
2021-10-31 14:19:14 -0400

    modified index a bit

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ include "./libraries.html";
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Focus Admin: Creative Admin Dashboard</title>
+    <title>SeaScan</title>
     <!-- ================= Favicon ================== -->
     <!-- Standard -->
     <link rel="shortcut icon" href="http://placehold.it/64.png/000/fff">
@@ -24,7 +24,7 @@ include "./libraries.html";
     <link rel="apple-touch-icon" sizes="57x57" href="http://placehold.it/57.png/000/fff">
     <!-- Styles -->
     <link href="assets/css/lib/calendar2/pignose.calendar.min.css" rel="stylesheet">
-    <!-- <link href="assets/css/lib/chartist/chartist.min.css" rel="stylesheet"> -->
+    <link href="assets/css/lib/chartist/chartist.min.css" rel="stylesheet">
     <link href="assets/css/lib/font-awesome.min.css" rel="stylesheet">
     <link href="assets/css/lib/themify-icons.css" rel="stylesheet">
     <link href="assets/css/lib/owl.carousel.min.css" rel="stylesheet" />
@@ -38,12 +38,13 @@ include "./libraries.html";
 <script>
     $(document).ready(function(){
         // $('#contentBody').load('./docForms/view_pageWithPdfAndOthers.php');  
-        $('#navContainer').load('./sideNav.php');  
+        // $('#navContainer').load('./sideNav.php');  
         $('#contentBody').load('./Home/view_home.php');  
     });
 </script>
 
-<body id="top">
+<body>
+
     <div class="sidebar sidebar-hide-to-small sidebar-shrink sidebar-gestures">
         <div class="nano">
             <div class="nano-content">
@@ -154,7 +155,6 @@ include "./libraries.html";
             </div>
         </div>
     </div>
-
     <!-- /# sidebar -->
 
     <div class="header">
@@ -366,7 +366,28 @@ include "./libraries.html";
 
     <div class="content-wrap">
         <div id="contentBody" class="main">
-
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-8 p-r-0 title-margin-right">
+                        <div class="page-header">
+                            <div class="page-title">
+                                <h1>Hello, <span>Welcome Here</span></h1>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /# column -->
+                    <div class="col-lg-4 p-l-0 title-margin-left">
+                        <div class="page-header">
+                            <div class="page-title">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+                                    <li class="breadcrumb-item active">Home</li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
@@ -375,7 +396,7 @@ include "./libraries.html";
     <script src="assets/js/lib/jquery.nanoscroller.min.js"></script>
     <!-- nano scroller -->
     <script src="assets/js/lib/menubar/sidebar.js"></script>
-    <!-- <script src="assets/js/lib/preloader/pace.min.js"></script> -->
+    <script src="assets/js/lib/preloader/pace.min.js"></script>
     <!-- sidebar -->
 
     <script src="assets/js/lib/bootstrap.min.js"></script>
@@ -386,19 +407,20 @@ include "./libraries.html";
     <script src="assets/js/lib/calendar-2/pignose.calendar.min.js"></script>
     <script src="assets/js/lib/calendar-2/pignose.init.js"></script>
 
-<!-- 
     <script src="assets/js/lib/weather/jquery.simpleWeather.min.js"></script>
-    <script src="assets/js/lib/weather/weather-init.js"></script> -->
+    <script src="assets/js/lib/weather/weather-init.js"></script>
     <script src="assets/js/lib/circle-progress/circle-progress.min.js"></script>
     <script src="assets/js/lib/circle-progress/circle-progress-init.js"></script>
-    <!-- <script src="assets/js/lib/chartist/chartist.min.js"></script> -->
-    <!-- <script src="assets/js/lib/sparklinechart/jquery.sparkline.min.js"></script>
-    <script src="assets/js/lib/sparklinechart/sparkline.init.js"></script> -->
+    <script src="assets/js/lib/chartist/chartist.min.js"></script>
+    <script src="assets/js/lib/sparklinechart/jquery.sparkline.min.js"></script>
+    <script src="assets/js/lib/sparklinechart/sparkline.init.js"></script>
     <script src="assets/js/lib/owl-carousel/owl.carousel.min.js"></script>
     <script src="assets/js/lib/owl-carousel/owl.carousel-init.js"></script>
+
     <!-- scripit init-->
     <!-- <script src="assets/js/dashboard2.js"></script> -->
 </body>
+
 </html>
 
 
@@ -419,4 +441,4 @@ include "./libraries.html";
     // if($stmt->execute()){
     //   echo "worked <br><br>";
     // }
-    ?>
+?>
